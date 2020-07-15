b1aa067766e5b591b66f3a96a5eb1617898a8565
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-14 21:50:37 -0700

    functions work

--- app/cart.js
+++ app/cart.js
@@ -1,13 +1,27 @@
-const cart = [{
-    id: 'londonCalling',
-    quantity: 2
-},
+//import data and scripts
 
-{
-    id: 'backInBlack',
-    quantity: 1
-}
+import cart from './cart-items.js';
+import tapes from './tapes.js';
 
-];
+import { renderLineItem } from './render-line-item.js';
+import { findByID, calcLinePrice, formatAsDollars, buildNavBar } from './store-utils.js';
 
-export default cart;
+// Get DOM elements
+
+const cartTable = document.querySelector('#table-body');
+const totalCell = document.querySelector('.total-box');
+let grandTotal = 0;
+//the magic happens here.
+buildNavBar('Cart');
+
+cart.forEach(cartItem => {
+    const product = findByID(cartItem.id, tapes);
+    const newRow = renderLineItem(product, cartItem.quantity);
+    cartTable.append(newRow);
+    grandTotal += calcLinePrice(product.price, cartItem.quantity);
+});
+
+totalCell.textContent = formatAsDollars(grandTotal);
+
+
+
--- app/store-utils.js
+++ app/store-utils.js
@@ -8,7 +8,7 @@ export function findByID(cartItem, productsArray) {
     return product;
 }
 
-export function calcLineItem(price, quantity) {
+export function calcLinePrice(price, quantity) {
     const rawTotal = Number(price) * Number(quantity);
     const total = Math.round(rawTotal * 100) / 100;
     return total;
@@ -17,4 +17,37 @@ export function calcLineItem(price, quantity) {
 export function formatAsDollars(num) {
     const roundedNumber = Math.round(Number(num) * 100) / 100;
     return `$${roundedNumber}`;
+}
+
+export function buildNavBar(page) {
+    const pages = [
+        {
+            name: 'Home',
+            url: 'index.html'
+        },
+        {
+            name:'Catalog',
+            url: 'catalog.html'
+        } , 
+        {
+            name: 'Cart',
+            url: 'cart.html'
+        },
+        {
+            name: 'Login',
+            url: './test/index.html'
+        }];
+    const navbar = document.querySelector('.nav-bar');
+    pages.forEach(item => {
+        const navAnchor = document.createElement('a');
+        const navLi = document.createElement('li');
+        
+        navAnchor.href = item.url;
+        if (page === item.name) navAnchor.className = 'current-nav';
+        navLi.textContent = item.name;
+        
+        navAnchor.appendChild(navLi);
+        navbar.appendChild(navAnchor);   
+    });    
+    // return navbar;
 }
