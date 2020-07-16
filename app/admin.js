8c8cec1280ce3c3c8072974d595e2e38d9d79173
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-16 14:25:22 -0700

    getting data from form

--- app/admin.js
+++ app/admin.js
@@ -1,2 +1,28 @@
+//import functions
 import { buildNavBar } from '../app/store-utils.js';
 buildNavBar('Admin');
+
+//get DOM elements
+const adminForm = document.getElementById('admin');
+
+//add event listeners and process data
+
+adminForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(adminForm);
+
+    const newTape = {
+        id: formData.get('id'),
+        title: formData.get('title'),
+        artist: formData.get('artist'),
+        description: formData.get('description'),
+        coverImg: formData.get('cover-img'),
+        genre: formData.get('genre'),
+        price: Number(formData.get('price')).toFixed(2),
+    
+    };
+   console.table(newTape); 
+});
+
+
+
