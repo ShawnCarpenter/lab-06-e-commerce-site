f22e95a844b09aaf4257ee17af7e73537856a209
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-16 15:41:57 -0700

    functions work, time to prettify

--- app/catalog.js
+++ app/catalog.js
@@ -1,11 +1,11 @@
 // import functions and grab DOM elements
 import { generateTapeList } from '../app/generateTapeList.js';
-import { buildNavBar } from '../app/store-utils.js';
-import tapes from '../app/tapes.js';
+import { buildNavBar, getTapes } from '../app/store-utils.js';
+// import tapes from '../app/tapes.js';
 const productList = document.querySelector('#product-list');
 
 // initialize state
-
+const tapes = getTapes();
 // set event listeners to update state and DOM
 buildNavBar('Catalog');
 tapes.forEach(tape => {
